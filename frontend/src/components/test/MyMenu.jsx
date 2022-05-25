070ec251b2f6909e7ba96f900808bc59251755c5
norbert <norbert.madauss@gmail.com>
2022-05-25 13:17:29 +0200

    test

---
+++ frontend/src/components/test/MyMenu.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { DashboardMenuItem, Menu, MenuItemLink } from "react-admin";
+import BookIcon from "@mui/icons-material/Book";
+
+export default function MyMenu(props) {
+  return (
+    <Menu {...props}>
+      <DashboardMenuItem primaryText="test" />
+      <MenuItemLink to="/test" primaryText="Trang 1" leftIcon={<BookIcon />} />
+      <MenuItemLink to="/test" primaryText="Trang 2" leftIcon={<BookIcon />} />
+      <MenuItemLink to="/test" primaryText="Andras 1" leftIcon={<BookIcon />} />
+      <MenuItemLink to="/test" primaryText="Andras 1" leftIcon={<BookIcon />} />
+      <MenuItemLink
+        to="/test"
+        primaryText="Norbert 1"
+        leftIcon={<BookIcon />}
+      />
+      <MenuItemLink
+        to="/test"
+        primaryText="Norbert 2"
+        leftIcon={<BookIcon />}
+      />
+      <MenuItemLink
+        to="/test"
+        primaryText="Norbert 3"
+        leftIcon={<BookIcon />}
+      />
+    </Menu>
+  );
+}

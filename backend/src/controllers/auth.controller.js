7d504f169a5f3b6860e6de562ef0400232ba00b0
Trang <ttphan.contact@gmail.com>
2022-06-13 11:42:46 +0200

    add secure flag for sending cookies to frontend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,7 +8,8 @@ class AuthController {
       const token = await AuthService.login(email, password);
 
       resData = { sucess: true };
-      res.cookie("user_token", token, { httpOnly: true });
+      // enable secure attribute later, ensures that cookies are only send via https
+      res.cookie("user_token", token, { httpOnly: true /* , secure: true  */ });
     } catch (err) {
       switch (err.name) {
         case "AuthentificationError":
@@ -58,7 +59,8 @@ class AuthController {
       const token = await AuthService.login(email, password);
 
       resData = { success: true, user };
-      res.cookie("user_token", token, { httpOnly: true });
+      // enable secure attribute later, ensures that cookies are only send via https
+      res.cookie("user_token", token, { httpOnly: true /* , secure: true  */ });
     } catch (err) {
       switch (err.name) {
         case "DuplicationError":

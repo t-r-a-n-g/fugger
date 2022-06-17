936ffb1dfa8049e1bfab9f306c20d23c123122c1
Max Dahl <github@daagur.de>
2022-06-17 13:30:51 +0200

    remove useless comment

--- backend/src/services/transfer.service.js
+++ backend/src/services/transfer.service.js
@@ -39,21 +39,6 @@ class TransferService {
     const datevAccounts = await DatevService.getUserAccounts(userId);
     const dbTransfers = [];
 
-    const monthIndex = {
-      Jan: 0,
-      Feb: 1,
-      Mrz: 2,
-      Apr: 3,
-      Mai: 4,
-      Jun: 5,
-      Jul: 6,
-      Aug: 7,
-      Sep: 8,
-      Okt: 9,
-      Nov: 10,
-      Dez: 11,
-    };
-
     for (const datevTransfer of transfers) {
       const userDatevAccount = datevAccounts.find((el) => {
         return el.number === datevTransfer.accountNumber;
@@ -61,7 +46,6 @@ class TransferService {
 
       if (userDatevAccount) {
         for (const transfer of datevTransfer.transfers) {
-          // const tDate = transfer.date.split("/"); // 0: month, 1: year
           const tDate = parseDate(transfer.date);
           dbTransfers.push({
             datevAccountId: userDatevAccount.id,

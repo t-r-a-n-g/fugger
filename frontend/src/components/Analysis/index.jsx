83233c77f8154a5ffa8f25412b566fe252973af0
Trang <ttphan.contact@gmail.com>
2022-07-07 15:12:11 +0200

    export button on analysis page

--- frontend/src/components/Analysis/index.jsx
+++ frontend/src/components/Analysis/index.jsx
@@ -8,6 +8,7 @@ import API from "@services/Api";
 
 import MonthRangePicker from "@components/MonthRangePicker";
 import AnTable from "./Table/AnTable";
+import ExportTable from "@components/ExportTable";
 
 import "./analysisTable.css";
 
@@ -430,6 +431,7 @@ class Analysis extends React.Component {
           onChange={this.onDateRangeChange}
           value={[analysisFrom, analysisTo]}
         />
+        <ExportTable />
         <AnTable data={data} headers={tableHeaders} />
       </>
     );

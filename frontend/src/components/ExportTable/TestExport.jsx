166e22816f493202568606552acf87608d08ab20
Trang <ttphan.contact@gmail.com>
2022-07-13 12:50:47 +0200

    styled exported excel table

--- frontend/src/components/ExportTable/TestExport.jsx
+++ frontend/src/components/ExportTable/TestExport.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "@mui/material";
 import useTableData from "@hooks/useTableData";
+/* import { round } from "@services/utils/math"; */
 
 import * as XLSX from "xlsx-js-style";
 
@@ -16,81 +17,208 @@ XLSX.set_fs(fs); */
 */
 
 function ExportTableTest() {
-  /*  hook useTableData */
-  const { categoryRows, /* subcategoryRows, datevRows,  */ headers } =
-    useTableData({
-      table: "analysis",
-    });
+  const { categoryRows, subcategoryRows, datevRows, headers } = useTableData({
+    table: "analysis",
+  });
 
-  /*  onClick for export */
+  function round(num) {
+    if (Number.isNaN(num)) return NaN;
+
+    const m = Number(num * 100).toPrecision(15);
+    const rounded = Math.round(m) / 100;
+
+    return Number.isNaN(rounded) ? 0 : rounded;
+  }
+
   const handleExport = () => {
     const rows = [];
 
-    /* HEADERS */
+    /* TABLE HEADERS */
     const monthsHeaderHelper = [""];
     const monthsArray = headers[0].slice(1).map((month) => month.value);
     for (let i = 0; i < monthsArray.length; i++) {
       monthsHeaderHelper.push(monthsArray[i], "", "", "");
     }
     const monthsHeader = monthsHeaderHelper.map((el) => {
-      return { v: el, t: "s", s: { font: { bold: true } } };
+      return {
+        v: el,
+        t: "s",
+        s: {
+          fill: { color: { rgb: "000000" } },
+          font: { bold: true, color: { rgb: "FFFFFF" } },
+        },
+      };
     });
 
     const subHeader = headers[1]
       .slice(1)
       .map((el) => (typeof el === "object" ? el.value : el))
       .map((el) => {
-        return { v: el, t: "s", s: { font: { bold: true } } };
+        return {
+          v: el,
+          t: "s",
+          s: {
+            fill: { color: { rgb: "000000" } },
+            font: { bold: true, color: { rgb: "FFFFFF" } },
+            border: { bottom: { style: "medium", color: { rgb: "FFFFFF" } } },
+          },
+        };
       });
 
     rows.push(monthsHeader, subHeader);
 
-    /* CATEGORIES, TO DO: rewrite this whole block to keep it easy */
-    /* console.log("Categories: ", categoryRows); */
-    const categoryArray = [];
-    for (let i = 0; i < categoryRows.length; i++) {
-      categoryArray.push(Object.values(categoryRows[i].cellData));
-    }
+    /* TABLE BODY, TO DO: change fill color */
 
-    const categoriesHelper = [];
-    for (const cat of categoryArray) {
-      // cat is an array of objects
-      const categ = [];
-      for (const c of cat) {
-        // c is an object
-        categ.push(c.value);
+    for (const category of categoryRows) {
+      const { id } = category;
+      const { cellData } = category;
+
+      const catRow = Object.values(cellData).map((el) =>
+        typeof el.value === "number" ? round(el.value) : el.value
+      );
+
+      const categoryStyled = catRow.map((el) => {
+        if (el > 0)
+          return {
+            v: el,
+            t: "s",
+            s: {
+              fill: { fgColor: { rgb: "819CDB" } },
+              font: { bold: true, color: { rgb: "228A3E" } },
+              border: { bottom: { style: "thin" }, top: { style: "medium" } },
+            },
+          };
+
+        if (el < 0)
+          return {
+            v: el,
+            t: "s",
+            s: {
+              fill: { fgColor: { rgb: "819CDB" } },
+              font: { bold: true, color: { rgb: "FF0000" } },
+              border: { bottom: { style: "thin" }, top: { style: "medium" } },
+            },
+          };
+
+        return {
+          v: el,
+          t: "s",
+          s: {
+            fill: { fgColor: { rgb: "819CDB" } },
+            font: { bold: true },
+            border: { bottom: { style: "thin" }, top: { style: "medium" } },
+          },
+        };
+      });
+
+      rows.push(categoryStyled);
+
+      for (const subcategory of subcategoryRows) {
+        const { categoryId } = subcategory;
+        const subcatId = subcategory.id;
+
+        if (id === categoryId) {
+          const subcatCellData = subcategory.cellData;
+
+          const subcatRow = Object.values(subcatCellData).map((el) =>
+            typeof el.value === "number" ? round(el.value) : el.value
+          );
+          const subcategoryStyled = subcatRow.map((el) => {
+            if (el > 0)
+              return {
+                v: el,
+                t: "s",
+                s: {
+                  fill: { fgColor: { rgb: "D0E5F8" } },
+                  font: { bold: true, color: { rgb: "228A3E" } },
+                  border: {
+                    bottom: { style: "thin" },
+                    top: { style: "medium" },
+                  },
+                },
+              };
+
+            if (el < 0)
+              return {
+                v: el,
+                t: "s",
+                s: {
+                  fill: { fgColor: { rgb: "D0E5F8" } },
+                  font: { bold: true, color: { rgb: "FF0000" } },
+                  border: {
+                    bottom: { style: "thin" },
+                    top: { style: "medium" },
+                  },
+                },
+              };
+
+            return {
+              v: el,
+              t: "s",
+              s: {
+                fill: { fgColor: { rgb: "D0E5F8" } },
+                font: { bold: true },
+                border: { bottom: { style: "thin" }, top: { style: "medium" } },
+              },
+            };
+          });
+
+          rows.push(subcategoryStyled);
+
+          for (const datevAcc of datevRows) {
+            const { subcategoryId } = datevAcc;
+
+            if (subcatId === subcategoryId) {
+              const datevCellData = datevAcc.cellData;
+              const datevRow = Object.values(datevCellData).map((el) =>
+                typeof el.value === "number" ? round(el.value) : el.value
+              );
+              const datevStyled = datevRow.map((el) => {
+                if (el > 0)
+                  return {
+                    v: el,
+                    t: "s",
+                    s: {
+                      font: { color: { rgb: "228A3E" } },
+                    },
+                  };
+
+                if (el < 0)
+                  return {
+                    v: el,
+                    t: "s",
+                    s: {
+                      font: { color: { rgb: "FF0000" } },
+                    },
+                  };
+
+                return {
+                  v: el,
+                  t: "s",
+                };
+              });
+
+              rows.push(datevStyled);
+            }
+          }
+        }
       }
-      categoriesHelper.push(categ);
     }
 
-    /* console.log("helper: ", categoriesHelper); */
-
-    const categories = categoriesHelper.map((row) =>
-      row.map((el) => {
-        return { v: el, t: "s", s: { fill: { fgColor: { rgb: "E9E9E9" } } } };
-      })
-    );
-
-    for (const cat of categories) {
-      rows.push(cat);
-    }
-
-    /* console.log("CATEGORIES ", categories);
-     */
-    /* console.log(rows); */
+    console.log("categories: ", categoryRows);
+    console.log("subcategories: ", subcategoryRows);
+    console.log(rows);
 
     // download the excel file
     if (rows.length > 0) {
       /* generate worksheet and workbook */
       const worksheet = XLSX.utils.aoa_to_sheet(rows);
-      /* console.log("worksheet: ", worksheet); */
       const workbook = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(workbook, worksheet, "Export_Fugger");
-      /* define headers of table */
-      /* XLSX.utils.sheet_add_aoa(worksheet, [monthsHeader], {
-        origin: "A1",
-      });
- */
+
+      /* change width of first column */
+      worksheet["!cols"] = [{ wch: 40 }];
+
       /* create an XLSX file and save as "anyfilename".xlsx */
       XLSX.writeFile(
         workbook,

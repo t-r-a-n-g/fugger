ae591ff099a398b6dc7aeb05ffd27490032c95b3
Norbert <norbert.madauss@gmail.com>
2022-07-13 11:57:32 +0200

    style fix for dark mode

--- frontend/src/components/budget/Amount.jsx
+++ frontend/src/components/budget/Amount.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import PropTypes from "prop-types";
-import { styled } from "@mui/material";
 import UserContext from "@contexts/UserContext";
 
 // TO DO: restrict input field to only 2 decimals and run validation check
@@ -20,34 +19,20 @@ export default function Amount(props) {
 
   const user = React.useContext(UserContext);
   const usedTheme = user.theme;
-  const CssTextField = styled(TextField)(({ theme }) => ({
-    "& label.Mui-focused": {
-      color:
-        usedTheme === "themeDark"
-          ? theme.palette.text.primary
-          : theme.palette.primary.main,
-    },
-    "& .MuiInput-underline:after": {
-      borderBottomColor: theme.palette.text.primary,
-    },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: theme.palette.text.secondary,
-      },
-      "&:hover fieldset": {
-        borderColor: theme.palette.text.primary,
-      },
-      "&.Mui-focused fieldset": {
-        borderColor:
-          usedTheme === "themeDark"
-            ? theme.palette.text.primary
-            : theme.palette.primary.main,
-      },
-    },
-  }));
 
   return (
-    <CssTextField
+    <TextField
+      color={usedTheme === "themeDark" ? "text" : null}
+      sx={
+        usedTheme === "themeDark"
+          ? {
+              "& .MuiInputLabel-root": { color: "text.primary" },
+              "& .MuiOutlinedInput-root": {
+                "& > fieldset": { borderColor: "text.primary" },
+              },
+            }
+          : null
+      }
       value={values.val[index].amount}
       id="outlined-basic"
       label="EUR"
